6c1d5c7aabb6dd834e9d11fe5474a09a1abd7e26
Geraldo I. Frazão de Araújo Neto <geraldoneto39@gmail.com>
2024-03-26 16:42:16 -0300

    commit

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,4 +1,5 @@
 // @ts-nocheck
+import { useState } from "react";
 import { FiLock, FiMail, FiUser } from "react-icons/fi";
 import { Input } from "../../components/Input";
 import { Background, Container, Form } from "./styles";
@@ -6,6 +7,16 @@ import { Button } from "../../components/Button";
 import { Link } from "react-router-dom";
 
 export function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSignUp() {
+    if(!name || !email || !password) {
+      return alert("Preencha todos os campos!");
+    }
+  };
+
   return(
     <Container>
 
@@ -21,21 +32,24 @@ export function SignUp() {
           placeholder="Nome"
           type="text"
           icon={FiUser}
+          onChange={e => setName(e.target.value)}
         />
 
         <Input 
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input 
           placeholder="Senha"
           type="password"
           icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title="Cadastrar"/>
+        <Button title="Cadastrar" onClick={handleSignUp}/>
         
         <Link to="/">Voltar ao ínicio</Link>
       </Form>
